4bc28e5c4195a8d73c00ea3a9cc576dd3c4923c1
wiut 00014232 <muqaddashudoyqulova@gmail.com>
2022-10-07 10:04:11 +0500

    init: encode

---
+++ main.py
@@ -0,0 +1,12 @@
+def encode (message):
+    encoded_message ="" \
+    i = 0
+    while (i <=len(message)-1):
+        count = 1
+        char = message [i]
+    encoded_message = encoded_message+char+str(count)
+    i = i+1 #i=+1
+    return  encoded_message
+
+encodeD_message = encode ("AAAAABBBBBB")
+print (encoded_message)
